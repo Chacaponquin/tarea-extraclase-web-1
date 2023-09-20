ff7b0f297a1056e506a8bbbc22f9ce79e481b2cf
JoseCarlosGarcia <josecgc1201@gmail.com>
2023-09-20 15:25:41 -0300

    Validaciones

--- js/coach.js
+++ js/coach.js
@@ -17,10 +17,32 @@ const PROVINCES = [
   "Ciego de Avila",
 ];
 
+const expresion_name = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+
 window.onload = () => {
   const teamSelect = document.getElementById("select-team");
   const dorsalInput = document.getElementById("coach-dorsal");
   const experienceInput = document.getElementById("coach-experience");
+  const nameInput = document.getElementById("input-name");
+  const formMns = document.getElementById("form_input-error");
+
+  const validateForm = (e) =>{
+      if(expresion_name.test(e.target.value)){
+        nameInput.classList.remove('input-name-error');
+        nameInput.classList.add('input-name');
+        formMns.classList.remove('formulario_input-error-active');
+        formMns.classList.add('form_input-error');
+      }else{
+        console.log("AAAAA");
+        nameInput.classList.remove('input-name');
+        nameInput.classList.add('input-name-error');
+        formMns.classList.remove('form_input-error');
+        formMns.classList.add('formulario_input-error-active');
+      }
+  };
+
+  nameInput.addEventListener('keyup', validateForm);
+  nameInput.addEventListener('blur', validateForm);
 
   for (const province of PROVINCES) {
     const option = document.createElement("option");
