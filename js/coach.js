78f96c53fc5a71cadef09ef6bd701369bc6f3305
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-09-20 18:25:24 -0400

    Remove validation methods

--- js/coach.js
+++ js/coach.js
@@ -17,31 +17,10 @@ const PROVINCES = [
   "Ciego de Avila",
 ];
 
-const expression_name = /^[a-zA-ZÀ-ÿ\s]{4,50}$/;
-
 window.onload = () => {
   const teamSelect = document.getElementById("select-team");
   const dorsalInput = document.getElementById("coach-dorsal");
   const experienceInput = document.getElementById("coach-experience");
-  const nameInput = document.getElementById("input-name");
-  const form_menssage = document.getElementById("form_input-error");
-
-  const validateForm = (e) =>{
-      if(expression_name.test(e.target.value)){
-        nameInput.classList.remove('input-name-error');
-        nameInput.classList.add('input-name');
-        form_menssage.classList.remove('formulario_input-error-active');
-        form_menssage.classList.add('form_input-error');
-      }else{
-        nameInput.classList.remove('input-name');
-        nameInput.classList.add('input-name-error');
-        form_menssage.classList.remove('form_input-error');
-        form_menssage.classList.add('formulario_input-error-active');
-      }
-  };
-
-  nameInput.addEventListener('keyup', validateForm);
-  nameInput.addEventListener('blur', validateForm);
 
   for (const province of PROVINCES) {
     const option = document.createElement("option");
